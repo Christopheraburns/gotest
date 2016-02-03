19c326d728f432d239a770c7041dbb6fa0f6d71b
Chris <christopheraburns@gmail.com>
2016-02-03 14:17:42 -0500

    minor changes

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	//fmt.Println(42)
+	//fmt.Printf("%d -%b - %x", 42, 42, 42)
+	//fmt.Printf("%d -%#X - %#x", 42, 42, 42)
+	for i := 0; i < 128; i++ {
+		fmt.Printf("%d \t %b \t %#x %q \n", i, i, i, i)
+	}
 }
